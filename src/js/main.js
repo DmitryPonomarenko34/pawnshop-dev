a02e2e95e4f5e380e5c3c327b4ff170c33d5e47a
SeniorDmitriy <dimaponomarenko443@gmail.com>
2022-04-14 13:08:29 +0300

    added product sec

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,20 @@
 import * as functions from './modules/functions.js';
+import Swiper, { Navigation } from 'swiper';
 
 functions.isWebp();
 
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // loop: false,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  modules: [Navigation,],
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 console.log('Джс работает!');
